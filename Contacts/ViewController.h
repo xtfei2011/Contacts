166399c7195cf7cacf8598d40b45caeb6f00fb64
xtfei_2011@126.com <xtfei1988712>
2019-03-07 09:18:37 +0800

    Initial Commit

---
+++ Contacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Contacts
+//
+//  Created by 谢腾飞 on 2019/3/7.
+//  Copyright © 2019 谢腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Contacts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Contacts
+//
+//  Created by 谢腾飞 on 2019/3/7.
+//  Copyright © 2019 谢腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
